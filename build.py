592510be58296a347c1478b80f9ca9d83215a29f
neri <neri@ac-0-f9-0-b1-e5.dhcp.lbl.gov>
2024-09-06 11:52:15 -0700

    Custom_install_script

---
+++ build.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import shutil
+from setuptools import setup
+from setuptools.command.install import install
+
+class CustomInstallCommand(install):
+    def run(self):
+        # Change to vendor directory
+        os.chdir('vendor')
+        
+        # Remove existing BBTools files
+        for item in os.listdir('.'):
+            if item.startswith('bb'):
+                if os.path.isdir(item):
+                    shutil.rmtree(item)
+                else:
+                    os.remove(item)
+        
+        # Download and extract BBTools
+        subprocess.run(['wget', 'https://sourceforge.net/projects/bbmap/files/latest/download', '-O', 'bbtools.tar.gz'], check=True)
+        subprocess.run(['tar', '-xf', 'bbtools.tar.gz'], check=True)
+        
+        # Change back to root directory
+        os.chdir('..')
+        
+        # Run the original install command
+        install.run(self)
+        
+        # Generate commands
+        subprocess.run(['generate-bbmapy-commands'], check=True)
+
+setup(
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+)
